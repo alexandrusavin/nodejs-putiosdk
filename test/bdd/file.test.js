1a69ebb53e6c8eeb4f32b0276cff6907dd062058
Alexandru Savin <alexandrusavin@gmail.com>
2016-04-05 23:22:56 +0200

    Return promise if callback not supplied

--- test/bdd/file.test.js
+++ test/bdd/file.test.js
@@ -1,27 +1,37 @@
 'use strict';
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
 
 const mockRequest = require('./fixtures/mocks').mock;
 
 const Client = require('../../index');
-
+const filesListFixture = require('./fixtures/filesList.json');
+const eventsFixture = require('./fixtures/events.json');
 
 describe('File list', () => {
 
     beforeEach(() => {
-        const fixture = require('./fixtures/filesList.json');
-        mockRequest.file.list(200, fixture, {parent_id: 123456});
+        mockRequest.file.list(200, filesListFixture, {parent_id: 123456});
         mockRequest.getDummy(200, {});
+        
+        mockRequest.file.events(200, eventsFixture);
 
         this.client = new Client('token');
-        this.expectedResult = fixture.files;
+        this.expectedFileListCallResult = filesListFixture;
+        this.expectedEventsCallResult = eventsFixture;
     });
 
-    it('should load file list', done => {
-        this.client.file.list({parent_id: 123456}, (err, result) => {
-            expect(result.files).to.deep.equal(this.expectedResult);
-            done();
-        });
+    it('should load file list', () => {
+        const result = this.client.file.list({parent_id: 123456});
+        return expect(result).to.eventually.deep.equal(this.expectedFileListCallResult);
+    });
+    
+    it('should return a promise if no callback passed', () => {
+        const result = this.client.file.events();
+        return expect(result).to.eventually.deep.equal(this.expectedEventsCallResult);
     });
 });
