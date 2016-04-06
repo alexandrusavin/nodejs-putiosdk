13ac97a770b262c0a582fb06e92afa8cdf7e5f0e
Alexandru Savin <alexandrusavin@gmail.com>
2016-04-06 18:28:52 +0200

    (Fixes #1) Download object should return a request object

--- test/bdd/file.test.js
+++ test/bdd/file.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Request = require('request').Request;
 const chai = require('chai');
 const chaiAsPromised = require("chai-as-promised");
 
@@ -19,6 +20,9 @@ describe('File list', () => {
         mockRequest.getDummy(200, {});
         
         mockRequest.file.events(200, eventsFixture);
+        mockRequest.file.download(302, '', {file_id: 54321}, {
+            Location: 'some://location'
+        });
 
         this.client = new Client('token');
         this.expectedFileListCallResult = filesListFixture;
@@ -33,5 +37,10 @@ describe('File list', () => {
     it('should return a promise if no callback passed', () => {
         const result = this.client.file.events();
         return expect(result).to.eventually.deep.equal(this.expectedEventsCallResult);
+    });
+
+    it('should return a request object for download method', () => {
+        const result = this.client.file.download({file_id: 54321});
+        expect(result).to.be.an.instanceof(Request);
     });
 });
--- test/bdd/fixtures/mocks.js
+++ test/bdd/fixtures/mocks.js
@@ -4,10 +4,14 @@ const nock = require('nock');
 const _ = require('lodash');
 const Router = require('named-routes');
 
+const routes = require('../../../util/routes');
+
 const router = new Router();
 const BASE_URL = 'https://api.put.io/v2/';
-const putioApi = nock(BASE_URL);
-const routes = require('../../../util/routes');
+const putioApi = nock(BASE_URL)
+    .defaultReplyHeaders({
+        'Content-Type': 'application/json'
+    });
 
 const mock = {
     getDummy: (statusCode, body) => {
@@ -24,15 +28,13 @@ const mock = {
 
 mock.file = {};
 _.map(routes, (route) => {
-    mock.file[route.name] = (statusCode, body, params) => {
+    mock.file[route.name] = (statusCode, body, params, headers) => {
         router.add(route.method.toLowerCase(), route.route, _.noop, {name: route.name});
         const apiPath = router.build(route.name, params);
 
         putioApi[route.method.toLowerCase()](apiPath)
             .query(true)
-            .reply(statusCode, body);
-
-
+            .reply(statusCode, body, headers);
     };
 });
 
